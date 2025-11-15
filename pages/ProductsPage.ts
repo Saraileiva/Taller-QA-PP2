8a0de08408a692ff69bc4aa8eafaaa52676eac55
sarahyleiva <00164021@uca.edu.sv>
2025-11-14 20:11:52 -0600

    Taller QA

---
+++ pages/ProductsPage.ts
@@ -0,0 +1,16 @@
+import { Page } from '@playwright/test';
+
+export class ProductsPage {
+readonly page: Page;
+readonly title;
+
+constructor(page: Page) {
+    this.page = page;
+    this.title = page.locator('.title');
+}
+
+async verifyIsOnProductsPage() {
+await this.title.waitFor();
+}
+}
+
